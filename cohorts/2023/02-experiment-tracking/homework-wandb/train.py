9b179b2aa2239318c66c48a00708cff4c0bfe409
Richard Saavedra <richardcmg7@gmail.com>
2023-06-07 02:04:20 +0000

    week2 task execution

--- cohorts/2023/02-experiment-tracking/homework-wandb/train.py
+++ cohorts/2023/02-experiment-tracking/homework-wandb/train.py
@@ -51,12 +51,15 @@ def run_train(
 
     mse = mean_squared_error(y_val, y_pred, squared=False)
     # TODO: Log `mse` to Weights & Biases under the key `"MSE"`
+    wandb.log({"MSE": mse})
 
     with open("regressor.pkl", "wb") as f:
         pickle.dump(rf, f)
 
     # TODO: Log `regressor.pkl` as an artifact of type `model`
-
+    model_artifact = wandb.Artifact("model", type="model")
+    model_artifact.add_file("regressor.pkl")
+    wandb.log_artifact(model_artifact)  
 
 if __name__ == "__main__":
     run_train()
